031bd79ac816267b26bb3116d6b9aa7046884279
aaronparisi <parisi.aaron@gmail.com>
2023-08-21 23:07:02 -0700

    initial repo request in App component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from 'react';
+import './stylesheets/App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    fetch('http://localhost:8080/repos')
+      .then((res) => res.json())
+      .then((json) => {
+        debugger;
+      });
+  }, []);
+
+  return <div className="App">Hello from app</div>;
 }
 
 export default App;
